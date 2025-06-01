b587dceb3903870b35c672cdc4c79ab523843b8d
deadchur <35635596+deadchur@users.noreply.github.com.>
2025-06-01 15:25:39 +1000

    Update: error correction for cleanup function

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const PlatARpus = () => {
     });
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+    camera.position.z = 2;
     camera.matrixAutoUpdate = false;
     cameraRef.current = camera;
     
@@ -146,6 +146,7 @@ const PlatARpus = () => {
       if (container && renderer.domElement) {
         container.removeChild(renderer.domElement);
       }
+      //subjectRef.current.unsubscribe();
       scene.clear();
     };
   }, []);
